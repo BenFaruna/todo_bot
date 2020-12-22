b672c5425138908a2ffa5ad09f0ae7c4da8c96f4
BenFaruna <benjaminfaruna@gmail.com>
2020-12-22 12:09:13 +0100

    Update bot.py
    Added inline bot functionality

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import logging
 import re
 import requests
 
-from telegram.ext import InlineQueryHandler, Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
 
 file = open('token.txt', 'r')
 TOKEN = file.read()
@@ -26,6 +27,7 @@ def echo(update, context):
 def bop(update, context):
     allowed_extension = ['jpeg', 'jpg', 'png', 'gif']
     file_extension = ''
+    url = ''
     while file_extension not in allowed_extension:
         contents = requests.get('https://random.dog/woof.json').json()
         url = contents['url']
@@ -37,11 +39,44 @@ def bop(update, context):
         context.bot.send_photo(chat_id=update.effective_chat.id, photo=url)
 
 
+def caps(update, context):
+    text_caps = " ".join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
 start_handler = CommandHandler('start', start)
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 bop_handler = CommandHandler('bop', bop)
+caps_handler = CommandHandler('caps', caps)
+inline_caps_handler = InlineQueryHandler(inline_caps)
+unknown_handler = MessageHandler(Filters.command, unknown)
+
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(bop_handler)
+dispatcher.add_handler(caps_handler)
+dispatcher.add_handler(inline_caps_handler)
+dispatcher.add_handler(unknown_handler)
 
 updater.start_polling()
+
+updater.idle()
